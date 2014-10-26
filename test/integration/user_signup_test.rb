17e8fddacd8dbde0e6828d3018ff58707bed4c14
Cheng Xian <chengxianlim@hotmail.com>
2014-10-27 00:21:00 +0800

    Finish log in/log out

--- test/integration/user_signup_test.rb
+++ test/integration/user_signup_test.rb
@@ -25,5 +25,6 @@ class UserSignupTest < ActionDispatch::IntegrationTest
                                            password_confirmation:password}      
     end
     assert_template 'users/show'
+    assert is_logged_in?
   end
 end
